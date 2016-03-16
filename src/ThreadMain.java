3709b0ca6cfe44a5a92f1bd63db4fb2889ee625d
Syanima <syanima@thoughtworks.com>
2016-03-16 12:26:40 +0530

    practices with java

---
+++ src/ThreadMain.java
@@ -0,0 +1,10 @@
+public class ThreadMain {
+    public static void main(String args[]) {
+
+        ThreadDemo T1 = new ThreadDemo( "Thread-1");
+        T1.start();
+
+        ThreadDemo T2 = new ThreadDemo( "Thread-2");
+        T2.start();
+    }
+}
